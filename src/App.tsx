40e1dcea6b03a80dafbef35a3a20e76b6c46b548
phuwinIV <phuwin.silachan@gmail.com>
2025-09-28 18:35:37 +0700

    fix home page not found

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import './App.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Navigate,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 
 const queryClient = new QueryClient();
@@ -16,11 +11,7 @@ function App() {
             <QueryClientProvider client={queryClient}>
                 <Router>
                     <Routes>
-                        <Route
-                            path="/"
-                            element={<Navigate to="/home" replace />}
-                        />
-                        <Route path="/home" element={<HomePage />} />
+                        <Route path="/" element={<HomePage />} />
                     </Routes>
                 </Router>
             </QueryClientProvider>
